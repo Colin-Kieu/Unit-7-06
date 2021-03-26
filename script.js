487c35084455a4719e6492c3ae4d1ad7ef817a45
ColinKieu <colin.kieu@ocsbstudent.ca>
2021-03-26 16:48:09 +0000

    linter

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let age = 15
+const age = 15
 let guess = 0
 // set our varibles to the values that are wanted
 
-while (guess !== age) {
+while (guess != age) {
   // the while loop
   guess = prompt('Guess my age')
-  if (guess = age) {
+  if (guess == age) {
     alert('Congratulations you got it right!')
   } else if (guess < age) {
     alert('You guessed too small, refresh to try again!')
